6aab09a3e4f435a19a3bad8325fad60529493b8b
FausFear <60197049+FausFear@users.noreply.github.com>
2024-01-16 02:26:46 +0500

    Add files via upload

---
+++ test_tenzor_site_3.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+from tenzor_site_3 import SbisDownloader
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope="module")
+def sbis_downloader():
+    url = "https://sbis.ru/"
+    sbis_downloader = SbisDownloader(url)
+    sbis_downloader.open_website()
+    sbis_downloader.download_sbis()
+    sbis_downloader.download_plugin()
+    yield sbis_downloader
+    sbis_downloader.close_browser()
+
+def test_download_file(sbis_downloader):
+    downloaded_file = sbis_downloader.download_file_name()
+    assert os.path.exists(downloaded_file)
+
+def test_file_size(sbis_downloader):
+    downloaded_file = sbis_downloader.download_file_name()
+    expected_size = float(
+        sbis_downloader.browser.find_elements(By.CSS_SELECTOR, ".sbis_ru-DownloadNew-loadLink__link.js-link")[
+            16].text.split(' ')[-2])
+    file_size = sbis_downloader.get_file_size(downloaded_file)
+    assert file_size == expected_size
+
+if __name__ == "__main__":
+    pytest.main()
